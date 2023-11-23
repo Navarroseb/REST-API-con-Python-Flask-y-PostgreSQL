cea1b3cece04184b5e8fdedf1921d7087ddbf4bd
Navarroseb <sebastnavarro@gmail.com>
2023-11-23 15:10:19 -0300

    GET Method

--- src/database/db.py
+++ src/database/db.py
@@ -2,6 +2,7 @@ import psycopg2
 from psycopg2 import DatabaseError
 from decouple import config
 
+
 def get_connection():
     try:
         return psycopg2.connect(
--- src/routes/movie.py
+++ src/routes/movie.py
@@ -1,8 +1,15 @@
 from flask import Blueprint, jsonify
 
+# Models
+from models.movieModel import MovieModel
+
 main = Blueprint('movie_blueprint', __name__)
 
 
 @main.route('/')
 def get_movies():
-    return jsonify({'message': "Hola soy un mensaje"})
+    try:
+        movies = MovieModel.get_movies()
+        return jsonify(movies)
+    except Exception as ex:
+        return jsonify({'message': str(ex)}), 500
